ca1396ab14cb62c2a720dad63efa4758c833138d
antonio.martinez <antonio.martinez@atmira.com>
2024-03-04 18:20:53 +0100

    Configure Kafka Producer and Consumer

---
+++ KafkaProvider/src/main/java/com/example/provider/KafkaProvider/config/KafkaTopicConfig.java
@@ -0,0 +1,30 @@
+package com.example.provider.KafkaProvider.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.config.TopicConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic generateTopic(){
+        Map<String, String> configurations = new HashMap<>();
+        // DELETE (Delete the message), COMPACT (Keep the most recent message)
+        configurations.put(TopicConfig.CLEANUP_POLICY_CONFIG, TopicConfig.CLEANUP_POLICY_DELETE);
+        configurations.put(TopicConfig.RETENTION_MS_CONFIG, "8640000"); // Message retention time, default -1
+        configurations.put(TopicConfig.SEGMENT_BYTES_CONFIG, "1073741824"); // 1 Gb. Maximum segment size, default 1 Gb
+        configurations.put(TopicConfig.MAX_MESSAGE_BYTES_CONFIG, "1000012"); // Maximum size of each message, default 1 MB
+
+        return TopicBuilder.name("Generated-Topic-1")
+                .partitions(2)
+                .replicas(2)
+                .configs(configurations)
+                .build();
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # springboot-kafka
 Example of using Spring Boot with Kafka queues.
+
+# **Pre-requisite**
+* Install Docker
+* Install Docker Compose
+
+# Run Kafka
+We will create a multi-node Kafka cluster setup, called kafka-docker-compose.yml
+
+Let’s spin up the cluster by using the docker-compose command.
+
+docker compose -f .\kafka-docker-compose.yml up -d
+
+Stop
+
+docker compose -f .\kafka-docker-compose.yml stop
+
+the image we use: http://wurstmeister.github.io/kafka-docker/
+
+Create new topic:
+
+docker-compose -f kafka-docker-compose.yml exec kafka kafka-topics.sh --create --topic Topic-2 --partitions 1 --replication-factor 1 --bootstrap-server kafka:9092
+
+List the created topic:
+
+docker-compose -f kafka-docker-compose.yml exec kafka kafka-topics.sh --list --bootstrap-server kafka:9092
+
